b6f897e12757169292ad03a13ad03bbcba8fd106
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-06-23 11:18:47 +0100

    add new favicon and title tab

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Leanne Goldsmith | Frontend Portfolio</title>
+        <link rel="icon" href="/LG_favicon.png" />
       </head>
       <body>
         {children}
